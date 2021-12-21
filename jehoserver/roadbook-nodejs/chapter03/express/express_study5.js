cb2db1135f1a74583654ca53f45200c712b42606
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-12-21 18:58:24 +0900

    Delete jehoserver/roadbook-nodejs directory

--- jehoserver/roadbook-nodejs/chapter03/express/express_study5.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('something broke!');
-});
-
-app.listen(8080);
--- jehoserver/roadbook-nodejs/chapter03/sample/simple_server2.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer((req,res) => {
-    console.log(req);
-    console.log(res);
-}).listen(8080, () => {
-    console.log('8080표트에서 서버 연결 중..')
-});

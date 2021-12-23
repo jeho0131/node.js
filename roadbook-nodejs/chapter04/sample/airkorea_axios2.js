d68e01dd845ba1cf4e6b61fd18f466c23381ddc8
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-12-23 18:58:41 +0900

    Add files via upload

--- roadbook-nodejs/chapter04/sample/airkorea_axios2.js
+++ roadbook-nodejs/chapter04/sample/airkorea_axios2.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* 라우팅 설정 */
-app.get('/airkorea', async (req, res) => {
+app.get('/airkorea/detail', async (req, res) => {
     const serviceKey = "vqwGV5MFkLSVrHUg6vWXaxEoXKNJTpwbVe5l0xEeg%2BlqG4YlWmeuM4W14FIYDZQc1LVaBgiNYpwYOmhEsqX9eg%3D%3D";
     const airUrl = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?";
 
@@ -21,16 +21,40 @@ app.get('/airkorea', async (req, res) => {
     parmas += '&' + encodeURI('pageNo') + '=' + encodeURI('1');
     parmas += '&' + encodeURI('dataTerm') + '=' + encodeURI('DAILY');
     parmas += '&' + encodeURI('ver') + '=' + encodeURI('1.3');
-    parmas += '&' + encodeURI('stationName') + '=' + encodeURI('종로구');
+    parmas += '&' + encodeURI('stationName') + '=' + encodeURI('마포구');
     parmas += '&' + encodeURI('returnType') + '=' + encodeURI('json')
 
     const url = airUrl + parmas;
-    console.log(url);
 
     try {
         const result = await axios.get(url);
-        res.json(result.data); // .data
-        console.log(result.data);
+        const airItem = {
+            "location": result.data.ArpltnInforInqireSvcVo["stationName"], // 지역
+            "time": result.data.list[0]['dataTime'], // 시간대
+            "pm10": result.data.list[0]['pm10Value'], // pm10 수치
+            "pm25": result.data.list[0]['pm25Value'], // pm25 수치
+        }
+        const badAir = [];
+        // pm10은 미세먼지 수치
+        if (airItem.pm10 <= 30) {
+            badAir.push("좋음😀");
+        } else if (pm10 > 30 && pm10 <= 80) {
+            badAir.push("보통😐");
+        } else {
+            badAir.push("나쁨😡");
+        }
+
+        //pm25는 초미세먼지 수치
+        if (airItem.pm25 <= 15) {
+            badAir.push("좋음😀");
+        } else if (pm25 > 15 && pm10 <= 35) {
+            badAir.push("보통😐");
+        } else {
+            badAir.push("나쁨😡");
+        }
+
+        res.send(`관측 지역: ${airItem.location} / 관측 시간: ${airItem.time} <br>
+        미세먼지 ${badAir[0]} 초미세먼지 ${badAir[1]} 입니다.`);
     } catch (error) {
         console.log(error);
     }
